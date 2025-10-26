bfbd2dae91b9525aa9ac5c58de785b1b247d5330
aurbini <aurbini92@gmail.com>
2025-10-25 22:50:29 -0400

    Log debug

--- src/db/client.ts
+++ src/db/client.ts
@@ -4,7 +4,7 @@ import postgres from 'postgres'
 import * as schema from './schema'
 
 const connectionString = process.env.DATABASE_POSTGRES_URL ?? ''
-
+console.log('connectionString', connectionString)
 // Disable prefetch as it is not supported for "Transaction" pool mode
 const client = postgres(connectionString, { prepare: false })
 export const db = drizzle(client, { schema })
